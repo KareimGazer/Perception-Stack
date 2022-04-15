2e297916efd4f856d0a79746cba852f2543d8122
kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>
2022-04-15 23:57:20 +0000

    get M_inv from bird view

--- .ipynb_checkpoints/bird_view-checkpoint.py
+++ .ipynb_checkpoints/bird_view-checkpoint.py
@@ -8,6 +8,7 @@ def get_bird_view(image):
     src_points = np.float32([[575, 460], [710, 460], [210, 690], [1080, 690]])
     dst_points = np.float32([[offset, 0], [width-offset, 0], [offset, height], [width-offset, height]])
     matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+    matrix_inv = cv2.getPerspectiveTransform(dst_points, src_points)
     out = cv2.warpPerspective(image, matrix, (width, height))
     return out
     
