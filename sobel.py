92d700ed14d0806046b7e5027730abce67318bd7
kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>
2022-04-15 18:00:05 +0000

    got some what good binary images

--- sobel.py
+++ sobel.py
@@ -87,3 +87,24 @@ def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
     binary_output[(arc_tan >= thresh_min) & (arc_tan <= thresh_max)] = 1
     return binary_output
 
+"""
+combines both sobel gradients on RGB images and color threshold on s channel in HLS image
+"""
+def get_binary(image, ksize):
+    gradx = abs_sobel_thresh(image, orient='x', sobel_kernel=ksize, thresh=(20, 100))
+    grady = abs_sobel_thresh(image, orient='y', sobel_kernel=ksize, thresh=(20, 100))
+    mag_binary = mag_thresh(image, sobel_kernel=ksize, mag_thresh=(30, 100))
+    dir_binary = dir_threshold(image, sobel_kernel=ksize, thresh=(0.7, 1)) # 0.7, 1.3 (old values)
+    combined_soble = np.zeros_like(dir_binary)
+    combined_soble[((gradx == 1) & (grady == 1)) | ((mag_binary == 1) & (dir_binary == 1))] = 1
+    
+    hls = cv2.cvtColor(image, cv2.COLOR_RGB2HLS)
+    s_channel = hls[:,:,2]
+    s_thresh_min = 150
+    s_thresh_max = 255
+    s_binary = np.zeros_like(s_channel)
+    s_binary[(s_channel >= s_thresh_min) & (s_channel <= s_thresh_max)] = 1
+    
+    combined_binary = np.zeros_like(combined_soble)
+    combined_binary[(s_binary == 1) | (combined_soble == 1)] = 1
+    return combined_binary
