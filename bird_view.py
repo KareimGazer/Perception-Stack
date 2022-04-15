2a02f2f3ca1b2f80da9dccb678967fa6f16aee28
kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>
2022-04-15 20:05:17 +0000

    added bird view with good confidence

---
+++ bird_view.py
@@ -0,0 +1,13 @@
+import numpy as np
+import cv2
+
+# pass matplot image
+def get_bird_view(image):
+    width, height = (image.shape[1], image.shape[0])
+    offset = 300
+    src_points = np.float32([[575, 460], [710, 460], [210, 690], [1080, 690]])
+    dst_points = np.float32([[offset, 0], [width-offset, 0], [offset, height], [width-offset, height]])
+    matrix = cv2.getPerspectiveTransform(src_points, dst_points)
+    out = cv2.warpPerspective(image, matrix, (width, height))
+    return out
+    
