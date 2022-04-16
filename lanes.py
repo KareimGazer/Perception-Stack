d4483f727326b73594b40273b8617f59ddb4acd7
kareim Tarek AbdelAzeem <kareimgazar1990@gmail.com>
2022-04-16 02:01:11 +0000

    refactored

--- lanes.py
+++ lanes.py
@@ -2,6 +2,7 @@ import numpy as np
 import cv2
 
 """
+takes image with one channel
 parameters
   nwindows: number of sliding windows
   margin: width of the windows +/- margin
@@ -81,10 +82,10 @@ def find_lane_pixels(binary_warped, nwindows=9, margin=100, minpix = 50):
     return leftx, lefty, rightx, righty, out_img
 
 
-def fit_polynomial(binary_warped):
+def fit_polynomial(binary_warped, nwindows=9, margin=100, minpix = 50):
     # Find our lane pixels first
-    # binary_warped = cv2.cvtColor(binary_warped, cv2.COLOR_RGB2GRAY)
-    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped)
+    binary_warped = cv2.cvtColor(binary_warped, cv2.COLOR_RGB2GRAY)
+    leftx, lefty, rightx, righty, out_img = find_lane_pixels(binary_warped, nwindows, margin, minpix)
 
     # Fit a second order polynomial to each using `np.polyfit`
     left_fit = np.polyfit(lefty, leftx, 2)
@@ -107,3 +108,24 @@ def fit_polynomial(binary_warped):
     out_img[righty, rightx] = [0, 0, 255]
 
     return out_img, left_fitx, right_fitx, ploty
+
+def draw_path(binary_warped, left_fitx, right_fitx, ploty, matrix_inv, undist):
+    binary_warped = cv2.cvtColor(binary_warped, cv2.COLOR_RGB2GRAY)
+    
+    # Create an image to draw the lines on
+    warp_zero = np.zeros_like(binary_warped).astype(np.uint8)
+    color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+    # Recast the x and y points into usable format for cv2.fillPoly()
+    pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+    pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+    pts = np.hstack((pts_left, pts_right))
+
+    # Draw the lane onto the warped blank image
+    cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+    # Warp the blank back to original image space using inverse perspective matrix (Minv)
+    newwarp = cv2.warpPerspective(color_warp, matrix_inv, (undist.shape[1], undist.shape[0])) 
+    # Combine the result with the original image
+    result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+    return result
